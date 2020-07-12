fb53bd13202d7f89e7e071499b002b9f24c0921c
Alex Solovev <alex.solovev.work@gmail.com>
2020-07-12 18:18:23 +0800

    Initial commit 🚀

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import Photos from './components/Photos'
+import { PhotosProvider } from './state/context'
+import { ReactComponent as SVGSprite } from './sprite.svg'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <PhotosProvider>
+        <Photos />
+      </PhotosProvider>
+      <SVGSprite />
+    </>
+  )
 }
 
-export default App;
+export default App
